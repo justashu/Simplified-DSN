bc1bd361df64226f3a4316d578be9be350816a30
justashu <yashu8115@gmail.com>
2024-03-24 19:58:48 +0000

    updated files

--- flip_gradient.py
+++ flip_gradient.py
@@ -1,24 +1,21 @@
 import tensorflow as tf
-from tensorflow.python.framework import ops
 
-
-class FlipGradientBuilder(object):
+class FlipGradientBuilder(tf.Module):
     def __init__(self):
+        super(FlipGradientBuilder, self).__init__()
         self.num_calls = 0
 
+    @tf.function
     def __call__(self, x, l=1.0):
         grad_name = "FlipGradient%d" % self.num_calls
         
-        @tf.RegisterGradient(grad_name)
-        def _flip_gradients(op, grad):
-            return [tf.negative(grad) * l]
+        @tf.custom_gradient
+        def _flip_gradients(x):
+            def grad(dy):
+                return tf.negative(dy) * l, None
+            return x, grad
         
-        g = tf.compat.v1.get_default_graph()
-        with g.gradient_override_map({"Identity": grad_name}):
-            y = tf.identity(x)
-            
         self.num_calls += 1
-        return y
-    
-flip_gradient = FlipGradientBuilder()
+        return _flip_gradients(x)
 
+flip_gradient = FlipGradientBuilder()
